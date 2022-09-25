3635a7b399adc900eef7a5f306fc875a155d0064
Krutika-Muley <91215900+Krutika-Muley@users.noreply.github.com>
2022-09-25 23:28:17 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
+using System;
+using System.Collections.Generic;
 
+namespace BMSSender
+{
+    class Program
+    {
+        private const float SOC_MIN = 0;
+        private const float SOC_MAX = 80;
+        private const float TEMP_MIN = -50;
+        private const float TEMP_MAX = 200;
+        private const int MAX_READING = 100;
+        static void Main(string[] args)
+        {
+            BatteryData.TempData = BatteryData.GenerateBatteryData(TEMP_MIN, TEMP_MAX, MAX_READING);
+            BatteryData.SOC = BatteryData.GenerateBatteryData(SOC_MIN, SOC_MAX, MAX_READING);
+            BatteryData.PrintBatteryData(BatteryData.TempData, BatteryData.SOC);
+        }
+    }
+ }
